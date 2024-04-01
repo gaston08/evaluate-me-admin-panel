41ff65c6256f5ad98862df64bbf40a371076e6c2
gaston pedraza <gaston08pedraza@gmail.com>
2024-04-01 14:52:40 -0300

    remove default values

--- src/app/contexts/Exercise.tsx
+++ src/app/contexts/Exercise.tsx
@@ -9,18 +9,9 @@ export const ExerciseContext = createContext<contextExercise>({
 // eslint-disable-next-line react-refresh/only-export-components
 export const defaultCurrentExercise = {
 	id: window.self.crypto.randomUUID(),
-	question: ['<p>titulo 1</p>', '<p>titulo 2</p>'],
+	question: ['<p></p>'],
 	correctOptions: [],
 	argument: '',
 	pts: '',
-	options: [
-		[
-			{ id: 1, title: 'hola', feedback: 'DEFAULT FEEDBACK' },
-			{ id: 2, title: 'option 2', feedback: '' },
-		],
-		[
-			{ id: 3, title: 'opt', feedback: '' },
-			{ id: 4, title: 'lastoption', feedback: '' },
-		],
-	],
+	options: [[]],
 };
