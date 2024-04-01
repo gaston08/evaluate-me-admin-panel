cedf856af0fd22e656303728da5f80ce22fc19ea
gaston pedraza <gaston08pedraza@gmail.com>
2024-04-01 15:00:58 -0300

    add argument preview

--- src/app/pages/exam/components/PreviewExercise/PreviewExercise.tsx
+++ src/app/pages/exam/components/PreviewExercise/PreviewExercise.tsx
@@ -1,10 +1,17 @@
 import { useContext } from 'react';
 import { ExerciseContext } from 'app/contexts/Exercise';
 import Exercise from 'app/pages/exam/components/Exercise';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 
 export default function PreviewExercise() {
 	const { currentExercise } = useContext(ExerciseContext);
 	return (
-		<Exercise exercise={currentExercise} canSelect={false} canEdit={true} />
+		<Box>
+			<Exercise exercise={currentExercise} canSelect={false} canEdit={true} />
+			<Box sx={{ mt: 3 }}>
+				<Typography color="green">{currentExercise.argument}</Typography>
+			</Box>
+		</Box>
 	);
 }
